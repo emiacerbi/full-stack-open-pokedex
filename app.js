5f9d93ef384019a8d80a589dfff76bf8374a25fa
Emiliano Acerbi <eacerbip@gmail.com>
2023-03-06 17:48:59 -0300

    throwing error on purpose

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5001
 
+app.get('/health', () => {
+  throw 'error...'
+})
+
+app.get('/version', (req, res) => {
+  res.send('2') // change this string to ensure a new version deployed
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
