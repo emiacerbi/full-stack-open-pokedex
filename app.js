e5b833f1a4e473877a4a35f90fd4bc9eeed59202
Emiliano Acerbi <eacerbip@gmail.com>
2023-03-06 18:42:49 -0300

    exercises 11.11 - 11.12

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5001
 
-app.get('/health', () => {
-  throw 'Error on purpose'
-  // res.send('ok')
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('3') // change this string to ensure a new version deployed
+  res.send('4')
 })
 
 app.use(express.static('dist'))
